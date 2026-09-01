187a7a0527caf7b915c2e8fe490742c6391c272d
Ignacio Kriman <63005850+iKriman@users.noreply.github.com>
2026-08-31 23:41:58 -0400

    Delete Exp1_S2_Grupo12 directory

--- Exp1_S2_Grupo12/Desarrollo-backend3-semana2-main/src/main/java/cl/duoc/backendiii/semana2/exception/DatoBancarioInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.backendiii.semana2.exception;
-
-public class DatoBancarioInvalidoException extends RuntimeException {
-    public DatoBancarioInvalidoException(String message) {
-        super(message);
-    }
-}
